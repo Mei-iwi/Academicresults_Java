7eedcb8d95f01bce1220ad4a33861d40b66b4d69
Mei-IwI <c2005vn@gmail.com>
2026-05-29 20:44:18 +0700

    remove import

--- src/main/java/com/academicresults/management/Controllers/StudentsController/StudentController.java
+++ src/main/java/com/academicresults/management/Controllers/StudentsController/StudentController.java
@@ -9,7 +9,6 @@ import jakarta.validation.Valid;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
